9330bacfec51611a5e95e089f96ff5000e8ee546
Cat Pien <catarina.pien@gmail.com>
2023-08-08 16:48:52 -0700

    saved theme code

---
+++ themes.R
@@ -0,0 +1,3 @@
+theme_plots <- theme_bw() + theme(axis.text = element_text(size = 12),
+                                  strip.text = element_text(size = 12),
+                                  legend.text = element_text(size = 12))
