4d3cb038fa49440b7110257281a796175fad7a0b
Cat Pien <catarina.pien@gmail.com>
2023-12-29 21:53:25 +0000

    Created parameters to help with automation by year

---
+++ parameters.R
@@ -0,0 +1,9 @@
+
+
+
+report_year = 2021
+wytype = "Critical"
+start = as.Date(paste0(report_year-10, "-01-01"))
+end = as.Date(paste0(report_year, "-12-31"))
+
+Sys.setenv(TZ='America/Los_Angeles')
