3c4de15665bdc9257281356c09eca2094554e036
Cat Pien <catarina.pien@gmail.com>
2024-04-01 10:30:53 -0700

    set up structure for pulling bio data

---
+++ get_bio_data.R
@@ -0,0 +1,24 @@
+library(dataRetrieval)
+library(sharpshootR)
+library(lubridate)
+library(dplyr)
+library(readr)
+library(here)
+source("functions.R")
+source("parameters.R")
+
+# Adult ------------------------------------
+
+
+# Egg to Fry --------------------------------
+
+
+# Juvenile ---------------------------
+
+## Upper Sac
+
+## Mid-low ----------------------------
+
+## Delta  ---------------------------
+
+
